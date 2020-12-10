86136fd2efced9b148f4dd7817bae6f8bc93f027
lmorais <leozonnn@gmail.com>
2020-12-10 14:10:15 -0300

    fix: corrections fixes

--- src/repositories/administrators.repository.js
+++ src/repositories/administrators.repository.js
@@ -3,5 +3,6 @@ const { Administrator } = require('../models');
 module.exports = {
   list: (query) => Administrator.findAndCountAll(query),
   getById: (id) => Administrator.findByPk(id),
+  get: (params) => Administrator.findOne({ where: params }),
   create: (params) => Administrator.create(params),
 };
