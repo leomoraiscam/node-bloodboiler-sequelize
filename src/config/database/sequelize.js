5ec6496f812ee0475ba56872e0bad8e72f65975e
lmorais <leozonnn@gmail.com>
2020-12-10 13:47:56 -0300

    fix: corrections fixes

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(__dirname, '../../../.env') });
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.NODE_ENV !== 'test' ? process.env.DB_NAME : process.env.DB_NAME_TEST,
+  database: process.env.NODE_ENV !== 'test' ? process.env.DB_NAME : `${process.env.DB_NAME}-test`,
   host: process.env.DB_HOST,
   dialect: 'postgres',
 };
