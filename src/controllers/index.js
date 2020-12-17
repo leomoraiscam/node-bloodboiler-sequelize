3e11d50620fa52886b416852e231e5315dc666be
lmorais <leozonnn@gmail.com>
2020-12-17 14:44:31 -0300

    refactor: movies

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,7 @@
 const usersController = require('./users.controller');
 const authController = require('./auth.controller');
 const administratorsController = require('./administrators.controller');
-const moviesController = require('./moviesController');
+const moviesController = require('./movies.controller');
 
 module.exports = {
   usersController,
