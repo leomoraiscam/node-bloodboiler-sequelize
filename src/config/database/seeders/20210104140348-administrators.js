1f45201d7f0dec2ce88668ba8afc28735b88a86b
lmorais <leozonnn@gmail.com>
2021-01-04 11:27:37 -0300

    feat: seeders

--- src/config/database/seeders/20210104140348-administrators.js
+++ src/config/database/seeders/20210104140348-administrators.js
@@ -4,13 +4,13 @@ module.exports = {
       'administrators',
       [
         {
-          id_user: 13,
+          id_user: 1,
           admin: 1,
           created_at: new Date(Date.now()),
           updated_at: new Date(Date.now()),
         },
         {
-          id_user: 14,
+          id_user: 2,
           admin: 1,
           created_at: new Date(Date.now()),
           updated_at: new Date(Date.now()),
